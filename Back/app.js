ed60e836e424a43e28b3dc6a2fd80dbd7dda773a
VeryAmazed <alan.h.zheng@outlook.com>
2022-07-18 23:49:22 -0700

    managed to get the website to send the image back. Code is messy and does not handle errors

--- Back/app.js
+++ Back/app.js
@@ -3,11 +3,13 @@ const path = require('path');
 const multer = require('multer');
 const fs = require('fs');
 
+//const bodyParser = require('body-parser');
 
 
 const app = express();
 const upload = multer({dest: 'uploads/'});
-app.use(express.json())
+app.use(express.json());
+//app.use(bodyParser.json());
 let id = 1;
 const id_to_images = new Map();
 /*
@@ -46,15 +48,50 @@ app.get('/id', (req,res)=>{
 
 
 app.post('/send', upload.single('image'), (req, res)=>{
-    
+    const curr_id = req.body.u_id;
     console.log(req.file, req.body);
-    if((req.body).u_id == -1){
-        res.status(400).end();
+    if(curr_id === -1){
+        res.status(400);
     }
-    id_to_images.set((req.body).u_id, [req.file.originalname, req.file.filename]);
-    console.log(id_to_images);
-    res.status(200);
-})
+    id_to_images.set(curr_id, [req.file.originalname, req.file.filename]);
+    
+    
+    // modify the images in here
+    // temp stand in code
+    fs.copyFile(`uploads/${id_to_images.get(curr_id)[1]}`, `uploads/after_${req.body.operation}${curr_id}.jpg`, (err)=>{
+        if(err){
+            console.log(err);
+            res.status(400);
+        }else{
+            id_to_images.get(curr_id).push(`after_${req.body.operation}${curr_id}.jpg`);
+            console.log(id_to_images);
+            res.status(200);
+            res.sendFile(path.join(__dirname, `uploads/${id_to_images.get(curr_id)[2]}`));
+            console.log("File sent");
+            console.log(path.join(__dirname, `uploads/${id_to_images.get(curr_id)[2]}`));
+        }
+        
+    });
+    
+    
+});
+/*
+app.post('/retrieve', (req, res)=>{
+    console.log("recieved");
+    //const curr_id = req.body.u_id;
+    const curr_id = 1;
+    //console.log(req.body);
+    //console.log(curr_id);
+    console.log(id_to_images.get(curr_id));
+    if(id_to_images.get(curr_id).length !== 3){
+        console.log("bad");
+        res.status(400);
+    }else{
+        res.sendFile(path.join(__dirname, `uploads/${id_to_images.get(curr_id)[2]}`));
+    }
+    
+});
+*/
 
 app.listen(8080);
 
--- Front/main.js
+++ Front/main.js
@@ -1,27 +1,78 @@
-
 const form = document.querySelector('form');
 const button = document.querySelector('button');
+const inFile = document.querySelector('#image');
+const radio = document.getElementsByName("operation");
 // form submits and then we hide main and make a fetch request
-let unique_id;
+let unique_id = -1;
 
 window.addEventListener('load', (e)=>{
     
     fetch('http://localhost:8080/id').then((resp) =>{
-        console.log("resp?");
+        //console.log("resp?");
         if(!resp.ok){
             
-            throw new Error('Http error! Status: ${resp.status}');
+            throw new Error(`Http error! Status: ${resp.status}`);
         }
         return resp.json();
     }).then(vals=>{
         unique_id = vals.value;
         document.querySelector('#u_id').value = unique_id;
+        console.log(unique_id);
     });
+    
 });
 
 form.addEventListener('submit', (e)=>{
+   e.preventDefault();
     document.querySelector('main').style.visibility = "hidden";
     document.querySelector("#waiting").style.visibility = "visible";
-    
-})
+    let result;
+    for(i = 0; i < radio.length; i++){
+        if(radio[i].checked){
+            result = radio[i].value;
+        }
+    }
+    const formData = new FormData();
+    formData.append("image", inFile.files[0]);
+    formData.append("operation", result);
+    formData.append("u_id", unique_id);
+    fetch("http://localhost:8080/send", {
+        method: "POST",
+        body: formData,
+    }).then((resp)=>{
+        console.log("made it here1");
+        
+        if(!resp.ok){
+            throw new Error(`HTTP error! Status: ${ response.status }`);
+        }
+        
+        //console.log(resp.blob());
+        return resp.blob();
+    }).then(resp =>{
+        console.log("made it here2");
+        console.log(resp);
+        const imgUrl =  URL.createObjectURL(resp);
+        document.querySelector('main').style.visibility = "visible";
+        document.querySelector("#waiting").style.visibility = "hidden";
+        document.querySelector("#after").src = imgUrl;
+    });
+    //console.log(JSON.stringify({u_id: unique_id}));
+    /*
+    fetch('http://localhost:8080/retrieve', {
+        method: 'POST',
+        body: JSON.stringify({u_id: unique_id}),
+    }).then(resp =>{
+        console.log("resp?");
+        if(!resp.ok){
+            document.querySelector("#waiting").style.visibility = "hidden";
+            document.querySelector("#error").style.visibility = "visible";
+            throw new Error(`Http error! Status: ${resp.status}. Please make sure you submit a valid image file with proper dimensions`);
+        }
+        return resp.blob();
+    }).then(blob => {
+        const objectURL = URL.createObjectURL(blob);
+        document.querySelector('#after').src = objectURL;
+    });
+    */
+});
 
